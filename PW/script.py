a25f1809749a4edd46e1e0436b59fc14439e205c
AustinPipkins <akpvmr@umsystem.edu>
2021-05-27 13:37:01 -0500

    commented

--- PW/script.py
+++ PW/script.py
@@ -5,15 +5,22 @@ import asyncio
 
 async def main():
     async with async_playwright() as p:
+        #construct browser environment
         browser = await p.webkit.launch()
         page = await browser.new_page()
+
+        #go to url of blazor site
         await page.goto("http://localhost:5000/")
+
+        #screenshot
         await page.screenshot(path="main.png")
         print("sd")
         await page.goto("http://localhost:5000/counter")
         print("sdf")
         await page.screenshot(path="counter.png")
         print("sdf")
+
+        #click button
         await page.click("text=Click me")
         await page.click("text=Click me")
         await page.click("text=Click me")
